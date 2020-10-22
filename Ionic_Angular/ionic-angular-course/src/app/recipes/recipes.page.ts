65147152076be9d61dbabed45e9e32d888498647
JuanMelendres <juan.mele97@gmail.com>
2020-10-22 11:58:23 -0500

    Update app routing
    Signed-off-by: JuanMelendres <juan.mele97@gmail.com>

--- Ionic_Angular/ionic-angular-course/src/app/recipes/recipes.page.ts
+++ Ionic_Angular/ionic-angular-course/src/app/recipes/recipes.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { Recipe } from './recipe.model';
 import { RecipesService } from './recipes.service';
@@ -8,12 +8,34 @@ import { RecipesService } from './recipes.service';
   templateUrl: './recipes.page.html',
   styleUrls: ['./recipes.page.scss']
 })
-export class RecipesPage implements OnInit {
+export class RecipesPage implements OnInit, OnDestroy {
   recipes: Recipe[];
 
   constructor(private recipesService: RecipesService) {}
 
   ngOnInit() {
+    console.log('ngOnInit');
+    console.log(this.recipes);
+  }
+
+  ionViewWillEnter() {
+    console.log('ionViewWillEnter');
     this.recipes = this.recipesService.getAllRecipes();
+  }
+
+  ionViewDidEnter() {
+    console.log('ionViewDidEnter');
+  }
+
+  ionViewWillLeave() {
+    console.log('ionViewWillLeave');
+  }
+
+  ionViewDidLeave() {
+    console.log('ionViewDidLeave');
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy');
   }
 }
